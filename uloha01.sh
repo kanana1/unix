44c4d4bad9ac70cb8ffd9ed5a70645e8aed128b6
Natalie Kanakova <naty.kanakova@gmail.com>
2019-03-11 21:16:18 +0100

    domaci ukol c. 1

---
+++ uloha01.sh
@@ -0,0 +1,31 @@
+if [ "$1" == "--typ" ]; then
+	if [ -e "$2" ]; then
+		if [ -f "$2" ]; then
+			echo "obycejny soubor"
+		elif [ -d "$2" ]; then
+			echo "adresar"
+		elif [ -b "$2" ]; then
+			echo "blokove zarizeni"
+		elif [ -c "$2" ]; then
+			echo "znakove zarizeni"
+		elif [ -l "$2" ]; then
+			echo "symbolicky link"
+		elif [ -p "$2" ]; then
+			echo "roura"
+		elif [ -s "$2" ]; then
+			echo "socket"
+		fi
+	else 
+		echo "Soubor neexistuje."
+		exit 1
+	fi	
+	exit 0
+elif [ "$1" == "--help" ]; then
+	echo "Pouziti: uloha01.sh [--typ|--help] [cesta_k_souboru]"
+	echo "--typ	vypise typ souboru"
+	echo "--help	vypise navod na pouziti"
+	echo "Exit status:"
+	echo "0 ... pokud soubor existuje"
+	echo "1 ... pokud soubor neexistuje"
+	exit 0
+fi	
