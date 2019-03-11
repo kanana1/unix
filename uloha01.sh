085d0434d401bb71840f8225b341187e9f41f727
Natalie Kanakova <naty.kanakova@gmail.com>
2019-03-11 21:50:26 +0100

    domaci ukol c. 1

--- uloha01.sh
+++ uloha01.sh
@@ -1,22 +1,22 @@
 if [ "$1" == "--typ" ]; then
 	if [ -e "$2" ]; then
 		if [ -f "$2" ]; then
-			echo "obycejny soubor"
+			echo "soubor"
 		elif [ -d "$2" ]; then
 			echo "adresar"
-		elif [ -b "$2" ]; then
-			echo "blokove zarizeni"
+		elif [ -L "$2" ]; then
+			echo "symbolicky link"
 		elif [ -c "$2" ]; then
 			echo "znakove zarizeni"
-		elif [ -l "$2" ]; then
-			echo "symbolicky link"
+		elif [ -b "$2" ]; then
+			echo "blokove zarizeni"
 		elif [ -p "$2" ]; then
 			echo "roura"
 		elif [ -s "$2" ]; then
 			echo "socket"
 		fi
 	else 
-		echo "Soubor neexistuje."
+		echo "Chyba"
 		exit 1
 	fi	
 	exit 0
